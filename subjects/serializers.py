91576e6ab0776b82348d9d9e2de187e9006f853b
Nicknamezz00 <1315098969@qq.com>
2022-03-18 20:05:40 +0800

    big problem, save for now

--- subjects/serializers.py
+++ subjects/serializers.py
@@ -1,6 +1,24 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
+
+from subjects.models import SubjectCategory_1, SubjectCategory_2, SubjectCategory_3
+
+
+class SubjectSerializer3(serializers.ModelSerializer):
+    class Meta:
+        model = SubjectCategory_3
+        fields = ['subject', 'level']
+
+
+class SubjectSerializer2(serializers.ModelSerializer):
+
+    class Meta:
+        model = SubjectCategory_2
+        fields = ['subject', 'level', 'level3_subjects']
 
 
 class SubjectSerializer(serializers.ModelSerializer):
+
     class Meta:
-        fields = '__all__'
+        model = SubjectCategory_1
+        fields = ['subject', 'level']
