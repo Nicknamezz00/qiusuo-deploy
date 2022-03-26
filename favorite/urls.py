b34f43fbecb69537c0a76ef31a308fe2914f8113
Nicknamezz00 <1315098969@qq.com>
2022-03-26 13:30:57 +0800

    fix style

--- favorite/urls.py
+++ favorite/urls.py
@@ -5,13 +5,13 @@ from favorite.views import favorite_api
 
 favorite_api_router = routers.DefaultRouter()
 
-favorite_api_router.register('favoritesfolder',
+favorite_api_router.register('favorite-folder',
                              favorite_api.UserFavoriteFolderViewSet,
-                             basename='userFavoritefolder')
+                             basename='favorite-folder')
 
-favorite_api_router.register('favorites',
+favorite_api_router.register('favorite',
                              favorite_api.UserFavoriteViewSet,
-                             basename='userFavorite')
+                             basename='favorite')
 urlpatterns = [
-    path('', include((favorite_api_router.urls, 'favorite'), namespace='favorites'))
+    path('', include((favorite_api_router.urls, 'favorite'), namespace='favorite'))
 ]
