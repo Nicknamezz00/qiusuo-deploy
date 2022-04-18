68be45fc4ce593d926367e64d3cf8f1fbb6bb705
Nicknamezz00 <1315098969@qq.com>
2022-04-18 18:47:45 +0800

    clean up.

--- informations/views.py
+++ informations/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 
 from informations.models import School
 from informations.serializers import SchoolSerializer
 
 
+@permission_classes([AllowAny])
 class SchoolViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
