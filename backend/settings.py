265a0d1577fb6605c985315bd514ca918630ece6
root <root@localhost.localdomain>
2022-04-25 21:56:44 +0800

    add uwsgi.py

--- backend/settings.py
+++ backend/settings.py
@@ -112,10 +112,10 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'LOCAL_qiusuo',
-            'USER': 'root',
-            'PASSWORD': 'root',
-            'HOST': '127.0.0.1',
+            'NAME': 'local_qiusuo',
+            'USER': 'wrz',
+            'PASSWORD': 'wrz',
+            'HOST': 'localhost',
             'PORT': '3306'
         },
     }
@@ -156,11 +156,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static'),
-]
+STATIC_ROOT = BASE_DIR/'static'
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, '/static'),
+#]
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
