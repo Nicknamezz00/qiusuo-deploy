c4ddbc0dc26c8d2f6d54620afec0ba0e9269decd
zzy <1348869642@qq.com>
2022-04-26 13:19:13 +0800

    v1.2

--- index.py
+++ index.py
@@ -2,14 +2,14 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-prefix = 'http://43.138.201.217/'
+prefix = 'https://api.qiusuo-mc.cn/'
 
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def root(request):
     return Response({
-        'api-version': '1.1',
+        'api-version': '1.2',
         'swagger-doc': prefix + 'doc/',
         'docs': prefix + 'docs/',
         'operation-manage': prefix + 'operation-manage/',
