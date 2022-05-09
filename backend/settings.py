84f7cc83357a2933d3f45d4c30993c49d0c745a7
Nicknamezz00 <1315098969@qq.com>
2022-05-09 19:56:29 +0800

    feat(backend.settings): Password length, timezone
    Set default password length minimum to 6.
    Set USE_TZ=False, disable auto
    detection.

--- backend/settings.py
+++ backend/settings.py
@@ -152,11 +152,11 @@ USE_TZ = False
 STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR/'static'
 STATIC_ROOT = [
-   os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
