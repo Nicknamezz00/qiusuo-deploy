2b7d2767b0d1aac65f20c310b3841b50e622b2c5
Nicknamezz00 <1315098969@qq.com>
2022-05-09 19:16:23 +0800

    feat(users.admin): use `fieldsets` to rearrange user admin layout.
    use `fieldsets` to rearrange user admin layout.

--- users/admin.py
+++ users/admin.py
@@ -5,11 +5,33 @@ from users.models import UserInfo
 
 @admin.register(UserInfo)
 class UserInfoAdmin(admin.ModelAdmin):
-    def save_model(self, request, obj, form, change):
-        if not change:
-            obj.set_password(form.data.get('password'))
-        super().save_model(request, obj, form, change)
+    readonly_fields = ('password',)
+
+    date_hierarchy = 'created_at'
+    actions = ['manual_authenticate']
+    search_fields = ['username']
+    show_full_result_count = False
+
+    # Order matters.
+    list_filter = ('is_manual_authenticated',
+                   'position',
+                   'subject__cate_name',)
+
+    # Order matters.
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password', 'phone', 'email',
+                       'intro', 'qq', 'school', 'age', 'avatar',
+                       ('last_name', 'first_name'), 'subject',
+                       'position', 'is_manual_authenticated')
+        }),
+    )
 
     @admin.action(description='执行人工认证')
     def manual_authenticate(self, request, queryset):
         queryset.update(is_manual_authenticated=True)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.set_password(form.data.get('password'))
+        super().save_model(request, obj, form, change)
