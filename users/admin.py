30b6d7c59cf8e8c1cb6f377dc8c0418bacfdfff3
Nicknamezz00 <1315098969@qq.com>
2022-05-09 18:32:10 +0800

    feat(users.admin): add authenticate action to user admin, fix raw password issue
    add authenticate action to user admin. fix password issue caused by
    unhashed raw password

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from users.models import UserInfo
 
 
 @admin.register(UserInfo)
-class UserInfoAdmin(UserAdmin):
-    pass
+class UserInfoAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.set_password(form.data.get('password'))
+        super().save_model(request, obj, form, change)
+
+    @admin.action(description='执行人工认证')
+    def manual_authenticate(self, request, queryset):
+        queryset.update(is_manual_authenticated=True)
