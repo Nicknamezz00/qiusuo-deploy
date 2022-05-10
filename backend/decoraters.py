8ba2b03f4b726f68f8615b5599ff0b8be78ee778
Nicknamezz00 <1315098969@qq.com>
2022-05-10 20:59:43 +0800

    fix(backend.decorator, post.permissions): pass safe methods
    Use decorators to handle safe methods more elegently.

---
+++ backend/decoraters.py
@@ -0,0 +1,34 @@
+from functools import wraps
+
+from rest_framework import viewsets
+from rest_framework.request import Request
+
+SAFE_ACTIONS = ('list',)
+SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS')
+
+
+def pass_safe_method(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        request = None
+        view = None
+        for arg in args:
+            # Argument not a class.
+            if isinstance(arg, Request):
+                request = arg
+            if isinstance(arg, viewsets.ModelViewSet):
+                view = arg
+        res = func(*args, **kwargs)
+        if res:
+            return res
+        assert request
+
+        safe: bool = True
+        # Safe methods?
+        safe &= request.method in SAFE_METHODS
+        # Safe actions?
+        if view:
+            safe &= view.action in SAFE_ACTIONS
+        return safe
+
+    return decorated
